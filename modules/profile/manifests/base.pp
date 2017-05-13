7c8bb45fcaad20d78e4bb6ab6f908d6c26dce93d
Jakub Krcma <jakub.krcma@heureka.cz>
2017-05-13 21:12:31 +0200

    base: add syslog replacement

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,3 +1,5 @@
 class profile::base {
+    include profile::syslog
     include profile::account::taiku
+
 }
---
+++ modules/profile/manifests/syslog.pp
@@ -0,0 +1,28 @@
+class profile::syslog {
+    # make sure that rsyslog is completely removed
+    package { 'rsyslog':
+        ensure => purged,
+        require => Service['rsyslog'],
+    }
+    service { 'rsyslog':
+        enable => false,
+        ensure => stopped,
+    }
+
+    # install and enable busybox-syslogd
+    package { 'busybox-syslogd':
+        ensure => latest,
+        require => Package['rsyslog'],
+        notify => Service['busybox-syslogd'],
+    }
+    service { 'busybox-syslogd':
+        enable => true,
+        ensure => running,
+        require => File['default.syslogd']
+    }
+    file { 'default.syslogd':
+        path => '/etc/default/busybox-syslogd',
+        ensure => file,
+        source => 'puppet:///modules/profile/syslog/default.syslogd',
+    }
+}
