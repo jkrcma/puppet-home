7590bc146cb2614fbe4b3c18bc9a3bd784fe9b66
Jakub Krcma <jakub.krcma@heureka.cz>
2017-05-13 22:18:17 +0200

    base: add fail-safe puppet binaries

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -2,4 +2,20 @@ class profile::base {
     include profile::syslog
     include profile::account::taiku
 
+    # puppet everything-is-wrong-case binaries
+    file {'/usr/local/bin/puppet-test':
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 4755,
+        source => "puppet:///modules/profile/puppet/${::architecture}/puppet-test",
+    }
+
+    file {'/usr/local/bin/puppet-apply':
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 4755,
+        source => "puppet:///modules/profile/puppet/${::architecture}/puppet-test",
+    }
 }
