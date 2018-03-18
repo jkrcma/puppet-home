9002fab10cb001d4c2b14402f8f53a2d4f5e1a5f
Jakub Krcma <jakub.krcma@heureka.cz>
2018-03-18 01:58:35 +0100

    packages: decrease apt update timer to 1 day

--- modules/profile/manifests/system/packages.pp
+++ modules/profile/manifests/system/packages.pp
@@ -33,7 +33,7 @@ class profile::system::packages ($gpg_key) {
 class profile::system::packages::apt {
     exec { 'apt-get update':
         command => "/usr/bin/apt-get update",
-        onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1 | tail -1 )) )) <= 604800 ))'"
+        onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1 | tail -1 )) )) <= 86400 ))'"
     }
 }
 
