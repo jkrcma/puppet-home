286bce8c7a42d7382c2b570b8b1648faa0cbde36
Jakub Krcma <jakub.krcma@heureka.cz>
2018-03-25 19:01:31 +0200

    packages: set debug loglevel to daily apt update

--- modules/profile/manifests/system/packages.pp
+++ modules/profile/manifests/system/packages.pp
@@ -33,7 +33,8 @@ class profile::system::packages ($gpg_key) {
 class profile::system::packages::apt {
     exec { 'apt-get update':
         command => "/usr/bin/apt-get update",
-        onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1 | tail -1 )) )) <= 86400 ))'"
+        onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1 | tail -1 )) )) <= 86400 ))'",
+        loglevel => debug,
     }
 }
 
