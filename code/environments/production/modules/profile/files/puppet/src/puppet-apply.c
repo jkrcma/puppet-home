691b135e1c31305a629089048cca3c073d5721a4
Jakub Krcma <jkrcma@gmail.com>
2021-07-17 13:25:56 +0200

    puppet: Add RUBYOPT=-W0 env to puppet binaries

--- code/environments/production/modules/profile/files/puppet/src/puppet-apply.c
+++ code/environments/production/modules/profile/files/puppet/src/puppet-apply.c
@@ -4,6 +4,8 @@
 int main() {
     int ret;
 
+    // Get rid of Ubuntu 20.04 Ruby deprecation warnings
+    setenv("RUBYOPT", "-W0", 1);
     setuid(0);
     setgid(0);
     ret = system("/usr/bin/puppet agent --verbose --onetime --no-daemonize --show_diff --ignorecache --no-usecacheonfailure");
--- code/environments/production/modules/profile/files/puppet/src/puppet-test.c
+++ code/environments/production/modules/profile/files/puppet/src/puppet-test.c
@@ -4,8 +4,10 @@
 int main() {
     int ret;
 
+    // Get rid of Ubuntu 20.04 Ruby deprecation warnings
+    setenv("RUBYOPT", "-W0", 1);
     setuid(0);
     setgid(0);
-    ret = system("/usr/bin/puppet agent -t --noop");
+    ret = system("/usr/bin/puppet agent --test --noop");
     return ret > -1 ? ret : 1;
 }
