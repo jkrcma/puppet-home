8e57b34da68614bf2bb87372189a9c324de03f39
Jakub Krcma <jkrcma@gmail.com>
2021-07-18 22:51:09 +0200

    Encrypt secrets with eyaml

--- code/environments/production/modules/profile/manifests/puppetmaster.pp
+++ code/environments/production/modules/profile/manifests/puppetmaster.pp
@@ -1,10 +1,17 @@
 class profile::puppetmaster {
-    package { 'puppetmaster':
+    package { ['puppetmaster', 'hiera-eyaml']:
         ensure => latest,
     }
 
     service { 'puppet-master':
         ensure => running,
         enable => true,
+    }
+
+    exec { 'create eyaml keys':
+        command => 'eyaml createkeys && chown -R puppet:puppet keys/',
+        path => '/usr/bin',
+        cwd => '/var/lib/puppet',
+        creates => '/var/lib/puppet/keys',
     }
 }
