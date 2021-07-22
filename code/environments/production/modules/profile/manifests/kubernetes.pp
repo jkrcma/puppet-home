34deb911adb7cc2ccb8b4ff2b67c767a2448c505
Jakub Krcma <jkrcma@gmail.com>
2021-07-22 16:19:37 +0200

    kubernetes: Set hardware=zigbee node label

--- code/environments/production/modules/profile/manifests/kubernetes.pp
+++ code/environments/production/modules/profile/manifests/kubernetes.pp
@@ -1,3 +1,13 @@
 class profile::kubernetes {
     include k3s::install
+
+    # node tag for ZigBee gateway
+    # TODO: this might not work for worker nodes in the future
+    exec { 'set label for ZigBee':
+        command => "kubectl label nodes ${facts['hostname']} home.taiku.cz/hardware=zigbee-controller",
+        path => '/usr/local/bin:/usr/bin:/bin',
+        unless => "kubectl label nodes ${facts['hostname']} --list | grep ^home.taiku.cz/hardware=zigbee-controller",
+        onlyif => 'lsusb -d 1cf1:0030',
+
+    }
 }
