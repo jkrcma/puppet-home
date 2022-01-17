c03ddca9ce267c51f7ba8d2baa6c783cf75d3f05
Jakub Krcma <jkrcma@gmail.com>
2022-01-17 22:06:17 +0100

    Cleanup site.pp in favor of ENC

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -1,48 +1,6 @@
-# Bare metal
-node puppet5 {
-    include role::puppetmaster
-}
+## Puppet site manifest
+## See enc.py and nodes.yaml for the ENC-based node catalog
 
-node /rpi-k8s-[0-9]+/ {}
-
-# LXC containers
-node puppet {}
-
-node dns {
-    include role::dns_server
-}
-
-node nas {
-    include role::nas
-}
-
-node torrent {
-    include role::torrent_box
-}
-
-node build {}
-
-node monitor {
-    include role::monitor
-}
-
-node mediabox {
-    include role::media_box
-}
-
-node kodi {
-    include role::kodi
-}
-
-node smtp {
-    include role::smtp_relay
-}
-
-node pihole {
-    include role::pihole
-}
-
-# Defaults
 node default {
     include role::noop
 }
