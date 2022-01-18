8f18eaf8e19fe1a16069760fa5e2782e302779c9
Jakub Krcma <jkrcma@gmail.com>
2022-01-18 02:55:15 +0100

    system: Force cleanup of default_user

--- code/environments/production/modules/profile/manifests/system/cleanup.pp
+++ code/environments/production/modules/profile/manifests/system/cleanup.pp
@@ -1,6 +1,6 @@
 class profile::system::cleanup (String $default_user) {
     exec { "delete ${default_user} user":
-        command => "killall -u ${default_user} && deluser --remove-all-files ${default_user}",
+        command => "killall -w -u ${default_user}; deluser --remove-all-files ${default_user}",
         onlyif => "id ${default_user}",
         path => ['/usr/sbin', '/usr/bin'],
     }
