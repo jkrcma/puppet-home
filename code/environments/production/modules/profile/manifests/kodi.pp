2ee0439543d2851d5b2ff1035379c6c872477e98
Jakub Krcma <jkrcma@gmail.com>
2022-01-18 10:00:07 +0100

    kodi: Add x11 systemd service

--- code/environments/production/modules/profile/manifests/kodi.pp
+++ code/environments/production/modules/profile/manifests/kodi.pp
@@ -3,7 +3,7 @@ class profile::kodi {
         source => 'puppet:///modules/profile/kodi/sources.list',
         fingerprint => '91E7EE5E',
     } ->
-    package { 'kodi':
+    package { ['kodi', 'xinit']:
         ensure => latest,
     }
 
@@ -15,11 +15,36 @@ class profile::kodi {
         groups => ['audio', 'video', 'input'],
     }
 
+    service { 'x11-kodi':
+        enable => true,
+        ensure => running,
+        require => Package['kodi'],
+    }
+
     file { '/home/kodi/.xinitrc':
         ensure => file,
         owner => kodi,
         group => kodi,
-        content => "kodi --standalone\n",
+        content => "MESA_LOADER_DRIVER_OVERRIDE=i965 kodi --standalone\n",
         require => User['kodi'],
+    }
+
+    file { '/etc/systemd/system/x11-kodi.service':
+        ensure => file,
+        source => 'puppet:///modules/profile/kodi/x11-kodi.service',
+        require => File['/home/kodi/.xinitrc'],
+        notify => Service['x11-kodi'],
+    }
+
+#    file { '/etc/X11/Xwrapper.config':
+#        ensure => file,
+#        content => "allowed_users=console\nneeds_root_rights=yes",
+#    }
+
+    file { '/etc/X11/xorg.conf.d':
+        ensure => directory,
+        recurse => true,
+        source => 'puppet:///modules/profile/kodi/xorg.conf.d',
+        before => Service['x11-kodi'],
     }
 }
