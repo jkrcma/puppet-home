d590ee26da1e8f76f776a3aeb478c4339acc263c
Jakub Krcma <jkrcma@gmail.com>
2022-01-24 23:49:00 +0100

    lxd: Add preseed class

--- code/environments/production/modules/profile/manifests/lxd.pp
+++ code/environments/production/modules/profile/manifests/lxd.pp
@@ -1,10 +1,34 @@
 class profile::lxd ($package_provider = undef) {
+    include profile::lxd::preseed
+
     if $package_provider == 'snap' {
         include ::snapd
+    } else {
+        fail("The only supported package provider for LXD is 'snap'.")
     }
 
     package { 'lxd':
         ensure => latest,
         provider => $package_provider,
+    }
+
+    file { ['/data', '/data/lxd']:
+        ensure => directory,
+    }
+}
+
+class profile::lxd::preseed ($trust_password = undef) {
+    $preseed_file = '/data/lxd-preseed.yml'
+    file { $preseed_file:
+        ensure => file,
+        mode => '0400',
+        content => template('profile/lxd/lxd-init.yml.erb'),
+    }
+
+    exec { 'lxd init with preseed':
+        path => '/snap/bin:/usr/bin',
+        command => "cat ${preseed_file} | lxd init --preseed",
+        require => File[$preseed_file],
+        unless => 'test -f /var/snap/lxd/common/state',
     }
 }
