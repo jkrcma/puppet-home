8f1f5abeda34ee765a28b6db0359f488efe6cc17
Jakub Krcma <jkrcma@gmail.com>
2022-09-11 18:53:48 +0200

    profile: Rework puppetmaster roles and profiles

--- code/environments/production/modules/profile/manifests/puppetmaster.pp
+++ code/environments/production/modules/profile/manifests/puppetmaster.pp
@@ -1,5 +1,9 @@
 class profile::puppetmaster {
-    package { ['puppetmaster', 'hiera-eyaml', 'git']:
+    include profile::puppetmaster::config
+
+    $homedir = $profile::puppetmaster::config::homedir
+
+    package { ['puppet-master', 'hiera-eyaml', 'git']:
         ensure => latest,
     }
 
@@ -8,10 +12,75 @@ class profile::puppetmaster {
         enable => true,
     }
 
-    exec { 'create eyaml keys':
-        command => 'eyaml createkeys && chown -R puppet:puppet keys/',
-        path => '/usr/bin',
-        cwd => '/var/lib/puppet',
-        creates => '/var/lib/puppet/keys',
+    # eyaml keys are in Ansible because of circular dependency
+    ['private_key.pkcs7.pem', 'public_key.pkcs7.pem'].each |$item| {
+        file { "${homedir}/keys/${item}":
+            ensure => file,
+            noop => true,
+            require => Package['puppet-master'],
+        }
     }
+}
+
+class profile::puppetmaster::primary {
+    include profile::puppetmaster
+    include profile::puppetmaster::config
+
+    $homedir = $profile::puppetmaster::config::homedir
+    $sync_dest_host = $profile::puppetmaster::config::sync_dest_host
+
+    file { "${homedir}/.ssh":
+        ensure => directory,
+        owner => puppet,
+        group => puppet,
+        mode => '0700',
+        require => Package['puppet-master'],
+    }
+
+    # SSH key is in Ansible, we don't want it to be dependent on eyaml
+    ['id_ed25519', 'id_ed25519.pub'].each |$file| {
+        file { "${homedir}/.ssh/${file}":
+            ensure => file,
+            owner => puppet,
+            group => puppet,
+            mode => '0600',
+            noop => true,
+            require => Package['puppet-master'],
+        }
+    }
+
+    $known_hosts_file = "${homedir}/.ssh/known_hosts"
+    exec { 'scan sync destination host key':
+        command => "/usr/bin/ssh-keyscan ${sync_dest_host} > ${known_hosts_file}",
+        creates => $known_hosts_file,
+        user => puppet,
+        require => Package['puppet-master'],
+    }
+
+    $cadir = "${homedir}/ssl/ca"
+    cron { 'puppet CA sync':
+        command => "/usr/bin/rsync -aq ${cadir}/ puppet@${sync_dest_host}:${cadir}",
+        user => puppet,
+        minut => '*/5',
+        require => Package['puppet-master'],
+    }
+}
+
+class profile::puppetmaster::secondary {
+    include profile::puppetmaster
+    include profile::puppetmaster::config
+
+    $homedir = $profile::puppetmaster::config::homedir
+    $sshkey = $profile::puppetmaster::config::sync_sshkey
+
+    ssh_authorized_key { "primary@puppetmaster":
+        ensure => present,
+        user => puppet,
+        type => 'ssh-ed25519',
+        key => $sshkey,
+    }
+}
+
+class profile::puppetmaster::config ($homedir = '/var/lib/puppet', $sync_sshkey = undef, $sync_dest_host = undef) {
+    # Dummy config class, might be used later for `puppet.conf`
 }
--- code/environments/production/modules/role/manifests/puppetmaster.pp
+++ code/environments/production/modules/role/manifests/puppetmaster.pp
@@ -3,3 +3,15 @@ class role::puppetmaster {
     include profile::system::lxc
     include profile::puppetmaster
 }
+
+class role::puppetmaster::primary {
+    include profile::base
+    include profile::system::lxc
+    include profile::puppetmaster::primary
+}
+
+class role::puppetmaster::secondary {
+    include profile::base
+    include profile::system::lxc
+    include profile::puppetmaster::secondary
+}
