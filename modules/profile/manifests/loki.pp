523f97db2124b3d3c1b953b6bb75f30f08bd69f4
Jakub Krcma <jkrcma@gmail.com>
2022-10-16 03:39:04 +0200

    Add loki and promtail profiles

---
+++ modules/profile/manifests/loki.pp
@@ -0,0 +1,20 @@
+class profile::loki {
+    package { 'loki':
+        ensure => latest,
+    }
+
+    service { 'loki':
+        enable => true,
+        ensure => running,
+        require => Package['loki'],
+    }
+
+    file { '/etc/loki/loki.yml':
+        ensure => file,
+        group => loki,
+        mode => '640',
+        source => 'puppet:///modules/profile/loki/loki.yml',
+        require => Package['loki'],
+        notify => Service['loki'],
+    }
+}
---
+++ modules/profile/manifests/promtail.pp
@@ -0,0 +1,30 @@
+class profile::promtail (Boolean $journald = true) {
+    package { 'promtail':
+        ensure => latest,
+    }
+
+    service { 'promtail':
+        enable => true,
+        ensure => running,
+        require => Package['promtail'],
+    }
+
+    @user { 'promtail':
+        groups => ['promtail'],
+        membership => minimum,
+    }
+
+    if $journald {
+        Package['promtail'] ->
+        User <| title == promtail |> { groups +> 'systemd-journal' }
+    }
+
+    file { '/etc/promtail/promtail.yml':
+        ensure => file,
+        group => promtail,
+        mode => '640',
+        source => 'puppet:///modules/profile/promtail/promtail.yml',
+        require => Package['promtail'],
+        notify => Service['promtail'],
+    }
+}
