cc60b823bf994875ef46776b8ac3413b7f9219c2
Jakub Krcma <jkrcma@gmail.com>
2022-10-21 22:01:19 +0200

    Set default File resource values

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,13 @@
 ## Puppet site manifest
 ## See enc.py and nodes.yaml for the ENC-based node catalog
 
+# Default values
+File {
+    mode => '0644',
+    owner => root,
+    group => root,
+}
+
 node default {
     include role::noop
 }
