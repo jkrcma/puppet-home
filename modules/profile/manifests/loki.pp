290177e4a1b28caf6770b642d172e5becacb5db4
Jakub Krcma <jkrcma@gmail.com>
2022-10-22 21:31:44 +0200

    loki: Add Alertmanager config

--- modules/profile/manifests/loki.pp
+++ modules/profile/manifests/loki.pp
@@ -9,6 +9,24 @@ class profile::loki {
         require => Package['loki'],
     }
 
+    file { '/etc/loki/alerts':
+        ensure  => directory,
+        #recurse => true,
+        #purge   => true,
+        #source  => 'puppet:///modules/profile/prometheus/alerts',
+        #notify => Service['prometheus'],
+        require => Package['loki'],
+    }
+
+    file { '/var/lib/loki/tmp-alerts':
+        ensure => directory,
+        owner => loki,
+        group => loki,
+        mode => '0770',
+        require => Package['loki'],
+        before => Service['loki']
+    }
+
     file { '/etc/loki/loki.yml':
         ensure => file,
         group => loki,
