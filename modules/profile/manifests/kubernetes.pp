ac09b18ddded826471180bd93606623c25c42ecf
Jakub Krcma <jkrcma@gmail.com>
2024-12-03 22:42:04 +0100

    kubernetes: Add hardware label for sonoff

--- modules/profile/manifests/kubernetes.pp
+++ modules/profile/manifests/kubernetes.pp
@@ -3,12 +3,18 @@ class profile::kubernetes {
 
     # node tag for ZigBee gateway
     # TODO: this might not work for worker nodes in the future
-    exec { 'set label for ZigBee':
-        command => "kubectl label nodes ${facts['hostname']} home.taiku.cz/hardware=zigbee-controller",
+    exec { 'set label for ConBee II':
+        command => "kubectl label nodes ${facts['hostname']} home.taiku.cz/hardware=conbee-ii",
         path => '/usr/local/bin:/usr/bin:/bin',
-        unless => "kubectl label nodes ${facts['hostname']} --list | grep ^home.taiku.cz/hardware=zigbee-controller",
+        unless => "kubectl label nodes ${facts['hostname']} --list | grep ^home.taiku.cz/hardware=conbee-ii",
         onlyif => 'lsusb -d 1cf1:0030',
+    }
 
+    exec { 'set label for Sonoff':
+        command => "kubectl label nodes ${facts['hostname']} home.taiku.cz/hardware=sonoff",
+        path => '/usr/local/bin:/usr/bin:/bin',
+        unless => "kubectl label nodes ${facts['hostname']} --list | grep ^home.taiku.cz/hardware=sonoff",
+        onlyif => 'lsusb -d 10c4:ea60',
     }
 
     # hacky way to reduce the verbosity of k3s logs
